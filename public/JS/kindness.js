e4f757447cef1f920ee92e3bbea0891648b35787
lauren levaccare <laurenlevaccare@icloud.com>
2021-01-17 14:21:20 -0600

    without node modules

---
+++ public/JS/kindness.js
@@ -0,0 +1,18 @@
+
+   // BACKUP WHEEL function witout GIF IMAGE
+// (() => {
+//     const wheel = document.querySelector('.wheel');
+//     const startButton = document.querySelector('.button');
+//     let deg = 360;
+
+//     startButton.addEventListener('click', () => {
+//         wheel.style.transition = 'all 10s ease-out';
+//         wheel.style.transform = `rotate(${deg}deg)`;
+//     });
+//     wheel.addEventListener('transitionend', () => {
+//         wheel.style.transition = 'none';
+//         const actualDeg = deg % 360;
+//         wheel.style.transform = `rotate(${actualDeg}deg)`;
+//     });
+// })();
+
