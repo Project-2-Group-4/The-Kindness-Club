d4fb4c471c7b2695eac295b683a5cc1ca9911ad5
lauren levaccare <laurenlevaccare@icloud.com>
2021-01-21 20:58:15 -0600

    new java

--- public/JS/kindness.js
+++ public/JS/kindness.js
@@ -1,41 +1,27 @@
-
-   // BACKUP WHEEL function witout GIF IMAGE
-// (() => {
-//     const wheel = document.querySelector('.wheel');
-//     const startButton = document.querySelector('.button');
-//     let deg = 360;
-
-//     startButton.addEventListener('click', () => {
-//         wheel.style.transition = 'all 10s ease-out';
-//         wheel.style.transform = `rotate(${deg}deg)`;
-//     });
-//     wheel.addEventListener('transitionend', () => {
-//         wheel.style.transition = 'none';
-//         const actualDeg = deg % 360;
-//         wheel.style.transform = `rotate(${actualDeg}deg)`;
-//     });
-// })();
+// let task_complete = document.getElementById = ('completed').addEventlister("checkbox", completeTask);
 
 
-//act of kindness data
-var data = [
-   {"label":"Act 1", "value":100, "act":"Compliment someone on their mask"},
-   {"label":"Act 2", "value":100, "act":"Call someone you care about"},
-   {"label":"Act 3", "value":100, "act":"Compliment someone on their mask"},
-   {"label":"Act 4", "value":100, "act":"Compliment someone on their mask"},
-   {"label":"Act 5", "value":100, "act":"Compliment someone on their mask"},
-   {"label":"Act 6", "value":100, "act":"Compliment someone on their mask"},
-   {"label":"Act 7", "value":100, "act":"Compliment someone on their mask"},
-   {"label":"Act 8", "value":100, "act":"Compliment someone on their mask"},
-   {"label":"Act 9", "value":100, "act":"Compliment someone on their mask"},
-   {"label":"Act 10", "value":100, "act":"Compliment someone on their mask"},
-];
+// let show_task = document.getElementById = ('completed').addEventlister("click", completeTask);
 
-//function for spinning the wheel 
-function spin(data){
-   container.on("click");
+var task_btn = document.getElementById('completed')
 
-}
+let task = document.getElementById("task");
+
+let points=document.getElementById("points")
+
+
+
+
+displayTask.addEventListener("toggle", function () {
+   if ("toggle") {
    
+      task.hide()
+   }
+   else {
+      task.show()
+      points.show()
+   }
+console.log("displayTask")
+})
 
-
+displayTask()
