9bf5a9515b15a80e3d1f7d86c149fabcaf8f8bb1
lauren levaccare <laurenlevaccare@icloud.com>
2021-01-21 21:15:28 -0600

    you better work

--- public/JS/kindness.js
+++ public/JS/kindness.js
@@ -26,32 +26,3 @@ console.log("displayTask")
 })
 
 displayTask()
-=======
-// function for getting kindness act to generate to page
-
- $("#act").on("click", function(event) {
-
-    event.preventDefault();
- 
-
-    var kindAct = {
-   kindness_Act: $("").val().trim().serializ(),
-   points: $("").val().trim().parseInt().serialize()
-     
-    };
-
-    // Send the Get request for the kindact and points assocaited.
-    $.ajax("", {
-      type: "GET",
-      data: kindAct
-    }).then(
-      function() {
-        console.log("Kindness Act Generated");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-
-  
-  });
->>>>>>> laurenfe
