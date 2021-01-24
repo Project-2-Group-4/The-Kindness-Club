c75ec4a4fc4952448de5f64b5788b8effe530bbe
Jessica Parker <jessicaleighparker3@gmail.com>
2021-01-24 15:15:18 -0600

    adding seed file for badges

---
+++ BadgeSeed.sql
@@ -0,0 +1,7 @@
+Use sequelize_passport;
+
+insert into Badges (BadgeName, PointsRequired, IconPath,createdAt, updatedAt) VALUES ("Nelson Mandela", 2000,"hands",CURDATE(), CURDATE());
+insert into Badges (BadgeName, PointsRequired, IconPath,createdAt, updatedAt) VALUES ("Mother Theresa", 1000,"prayer",CURDATE(), CURDATE());
+insert into Badges (BadgeName, PointsRequired, IconPath,createdAt, updatedAt) VALUES ("Princess Diana", 500,"princess",CURDATE(), CURDATE());
+insert into Badges (BadgeName, PointsRequired, IconPath,createdAt, updatedAt) VALUES ("Lori", 600,"computer",CURDATE(), CURDATE());
+insert into Badges (BadgeName, PointsRequired, IconPath,createdAt, updatedAt) VALUES ("RockStar", 100,"star",CURDATE(), CURDATE());
