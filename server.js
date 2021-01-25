2e642ab9285f521eea06bb8eed5c507383e8ece7
Fabian <fabianffc@gmail.com>
2021-01-24 19:06:55 -0600

    Procfile

--- server.js
+++ server.js
@@ -6,21 +6,6 @@ const bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 const env = require('dotenv').config({path:'./.env'});
 
-//conection to jawsDB
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
