42f149a4b39c8ede709416809005c8523f9d87f1
Jessica Parker <jessicaleighparker3@gmail.com>
2021-01-25 17:40:53 -0600

    added css and user story to app

--- public/JS/kindness.js
+++ public/JS/kindness.js
@@ -5,12 +5,17 @@ $(document).ready(function () {
    var newPoints = {
      points: 100
    }
+
+   
    $.ajax({
      url: "/api/user", 
      method: "PUT",
-     data: newPoints
+     data: newPoints 
+     
    }).then(function(){
      window.location.reload()
+     newPoints 
+     
    })
  })
 
@@ -18,25 +23,27 @@ $(document).ready(function () {
   $.get("/api/task", function (task) {
     console.log("task", task);
 
-
     var elTask = document.getElementById('daily-task')
     elTask.textContent = task.TaskName
 
    
 
     task.onclick = function () {
-      // append points to page for user id logged in
-      // this is keeping track of points and awarding them to the user id logged in
-      //added default value of 100 in task.js for models
+
+      
       var elPoints = document.getElementById('points')
       elPoints.textContent = "Worth:"+ Tasks.pointsEarned
+
       // new task is shown in div by refreshing the page
       window.location.reload()
+
+
+
 
       /* 
       append task to page ---DONE
       get random task to append too page --- DONE
-      get point value to append to page
+      get point value to append to page -- DONE
       event handler to mark task complete to get points 
       button clicked =points are earned and new task is populated on kindness page -- DONE
       user id and task id are written to table 
