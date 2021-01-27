963b5b3fd530e8d41294f66ff8d252087796ca44
Fabian <fabianffc@gmail.com>
2021-01-26 20:48:53 -0600

    heroku app

---
+++ config/conection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'nameOfYour_db'
+    })
+};
