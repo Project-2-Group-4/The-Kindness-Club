9eb7c477c20afb50fa6978c126e6e2bb092e434d
PJ Dalton <patrickjdalton1@gmail.com>
2021-01-28 16:19:35 -0600

    Adding heroku files

--- config/config.js
+++ config/config.js
@@ -1,22 +1,23 @@
+require("dotenv").config();
+
 module.exports = {
-    "development": {
-      "username": "root",
-      "password": "password1",
-      "database": "sequelize_passport",
-      "host": "127.0.0.1",
-      "port": 3306,
-      "dialect": "mysql"
-    },
-    "test": {
-      "username": "root",
-      "password": "password1",
-      "database": "sequelize_passport",
-      "host": "127.0.0.1",
-      "port": 3306,
-      "dialect": "mysql"
-    },
-    "production": {
-      "use_env_variable": "JAWSDB_URL",
-      "dialect": "mysql"
-    }
+  "development": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
   }
+}
