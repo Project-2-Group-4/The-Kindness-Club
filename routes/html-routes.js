b6befd7ca1874ac186afddfe6caedef705e37270
PJ Dalton <patrickjdalton1@gmail.com>
2021-01-31 15:37:20 -0600

    Reworking routes for duplicate header bug

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,18 +10,20 @@ module.exports = function (app) {
     });
 
     app.get("/signin", function (req, res) {
-        res.render("signin", {error: req.flash('error')});
-        if (req.user) {
+        if(typeof req.user === "undefined"){
+            res.render("signin", {error: req.flash('error')});
+        }else{
             res.redirect("/kind");
         }
     });
 
     app.get("/register", function (req, res) {
         // If the user already has an account send them to the login page
-        if (req.user) {
+        if (typeof req.user === "undefined") {
+            res.render("register", {error: req.flash('error')});
+        }else{
             res.redirect("/kind");
         }
-        res.render("register", {error: req.flash('error')});
     });
 
     app.get("/kind", function (req, res) {
